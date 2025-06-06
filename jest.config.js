52f11006335c29d6972a182914d17baaade8d7f4
Sean Broome <bsean4890@gmail.com>
2025-06-05 21:19:07 -0400

    UPDATE

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,17 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['**/tests/**/*.(test|spec).(ts|tsx|js)'],
+  testMatch: ['**/tests/**/*.(test|spec).(ts|tsx|js|jsx)'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.json',
     },
   },
+  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@testing-library|@babel/runtime|some-esm-package)/)'
+  ],
 };
